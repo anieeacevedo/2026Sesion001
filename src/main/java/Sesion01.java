b4789a2e3fee2593bdc83c2dbdf703c0ef815708
Aniee Ac. <aniee.acevedo@gmail.com>
2026-04-24 10:23:30 -0500

    Creado Sesion01

---
+++ src/main/java/Sesion01.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sesion01;
+import java.util.Scanner;
+import modelo.Operacion;
+/**
+ *
+ * @author F20LAB207E06
+ */
+public class Sesion01 {
+
+    public static void main(String[] args) {
+        //void es que no regresa ningun valor,es publico y es static
+        //System es una clase,out objeto y println el metodo
+        Scanner teclado=new Scanner(System.in);
+        //uso la clase scanner 
+        Operacion operacion= new Operacion();
+        //al estar en paquuetes distintos no se conocen,tengo que importar
+        System.out.println("Ingrese l primer valor");
+        int numero=teclado.nextInt();
+        operacion.setOperador1(numero);
+         numero=teclado.nextInt();
+        operacion.setOperador2(numero);
+        System.out.println("La suma es "+ operacion.sumar());
+        System.out.println("La resta es "+ operacion.restar());
+        System.out.println("La multiplicacion es "+ operacion.multiplicar());
+        
+    }
+}
+//un paquete es una carpeta para odenar mis clases
